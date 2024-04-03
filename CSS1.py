3a4670c983329f484011d7ce020fabc2cd22559a
Karlson Thien <prolexicgt@gmail.com>
2024-04-03 14:19:34 +0800

    Added Roles to Customer.txt

--- CSS1.py
+++ CSS1.py
@@ -1,33 +1,37 @@
-choice = input("Would you like to create or update a customer account? (Create/Update)\n>>> ").lower()
-if choice == 'create':
-    print("Succesfully Created")
+def CSS1():
+    choice = input("Would you like to create or update a customer account? (Create/Update)\n>>> ").lower()
+    if choice == 'create':
+        print("Succesfully Created")
 
-elif choice == 'update':
-    # Check for invalid search value
-    nameCheck = 0
+    elif choice == 'update':
+        # Check for invalid search value
+        nameCheck = 0
 
-    with open('customer.txt') as customer_file:
-        accounts = customer_file.readlines()
+        with open('customer.txt') as customer_file:
+            accounts = customer_file.readlines()
 
 
-        # Modify data
-        for index, line in enumerate(accounts):
-            data = line.split(';')
-            del data[-1]
-            if data[1] == "Hussain Rana": #Check for inputted name
-                data[1] = "Pavin Raj" #Updated Name
-                accounts[index] = ';'.join(data)
+            # Modify data
+            for index, line in enumerate(accounts):
+                data = line.split(';')
+                # del data[-1]
+                print(data)
+                if data[1] == "Hussain Rana": #Check for inputted name
+                    data[1] = "Pavin Raj" #Updated Name
+                    accounts[index] = ';'.join(data)
+                else:
+                    nameCheck += 1
+
+            if nameCheck == len(accounts):
+                print("Invalid Name")
             else:
-                nameCheck += 1
-
-        if nameCheck == len(accounts):
-            print("Invalid Name")
-        else:
-            print("Name succesfully updated!")
+                print("Name succesfully updated!")
 
 
-    # Write modified data back to file
-    with open('customer.txt', 'w') as customer_file:
-        for line in accounts:
-            customer_file.write(f'{line}')
-            # print(line.split(';'))
+        # Write modified data back to file
+        with open('customer.txt', 'w') as customer_file:
+            for line in accounts:
+                customer_file.write(f'{line}')
+                # print(line.split(';'))
+
+CSS1()
