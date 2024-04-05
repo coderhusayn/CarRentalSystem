d4431311a95ae08077bef54313be0bf911e11372
Java <yewshan.ooi@gmail.com>
2024-04-05 13:29:14 +0800

    feat: added Option 2.5 - Update Rental Availability

--- carServiceStaff.py
+++ carServiceStaff.py
@@ -26,8 +26,8 @@ def CarSerStaff():
     MIN_CAPACITY = 1
     MAX_CAPACITY = 11
 
-    ERR_FILE_NOT_FOUND = '\n\033[0;31m\033[1mError: The .txt file does not exist.\033[0m\n'
-    ERR_FILE_NO_PERM = '\n\033[0;31m\033[1mError: You do not have permission to access the .txt file.\033[0m\n'
+    ERR_FILE_NOT_FOUND = '\n\033[0;31;1mError: The .txt file does not exist.\033[0m\n'
+    ERR_FILE_NO_PERM = '\n\033[0;31;1mError: You do not have permission to access the .txt file.\033[0m\n'
 
     def InputDate(prompt):
         while True:
@@ -36,18 +36,18 @@ def CarSerStaff():
                 day, month, year = date.split('-')
 
                 if len(day) != 2 or not day.isdigit() or int(day) < 1 or int(day) > 31:
-                    print('\033[0;31m\033[1mError: Please enter a day between 01 and 31.\033[0m')
+                    print('\033[0;31;1mError: Please enter a day between 01 and 31.\033[0m')
                     continue
                 elif len(month) != 2 or not month.isdigit() or int(month) < 1 or int(month) > 12:
-                    print('\033[0;31m\033[1mError: Please enter a month between 01 and 12.\033[0m')
+                    print('\033[0;31;1mError: Please enter a month between 01 and 12.\033[0m')
                     continue
                 elif len(year) != 4 or not year.isdigit() or int(year) < START_YEAR or int(year) > END_YEAR:
-                    print(f'\033[0;31m\033[1mError: Please enter a four digit year between {START_YEAR} and {END_YEAR}.\033[0m')
+                    print(f'\033[0;31;1mError: Please enter a four digit year between {START_YEAR} and {END_YEAR}.\033[0m')
                     continue
                 else:
                     break
             except ValueError:
-                print('\033[0;31m\033[1mError: Please enter a date in the format of DD-MM-YYYY.\033[0m')
+                print('\033[0;31;1mError: Please enter a date in the format of DD-MM-YYYY.\033[0m')
         return date
 
     def InputFloat(prompt):
@@ -55,7 +55,7 @@ def CarSerStaff():
             try:
                 flt = float(input(prompt))
             except ValueError:
-                print('\033[0;31m\033[1mError: Please enter a valid float.\033[0m')
+                print('\033[0;31;1mError: Please enter a valid float.\033[0m')
                 continue
             else:
                 truncated_flt = format(flt, '.2f')
@@ -67,7 +67,7 @@ def CarSerStaff():
         option = input('>>> ').lower()
 
         if option not in [CSS_REGISTER, CSS_UPDATE, CSS_VIEW, CSS_PROFILE, CSS_LOGOUT]:
-            print(f'\033[0;31m\033[1mError: Please enter {CSS_REGISTER}, {CSS_UPDATE}, {CSS_VIEW}, {CSS_PROFILE}, or {CSS_LOGOUT} as an option.\033[0m')
+            print(f'\033[0;31;1mError: Please enter {CSS_REGISTER}, {CSS_UPDATE}, {CSS_VIEW}, {CSS_PROFILE}, or {CSS_LOGOUT} as an option.\033[0m')
         else:
             # Option 1 - Register a new car
             if option == CSS_REGISTER:
@@ -84,11 +84,11 @@ def CarSerStaff():
                         try:
                             year_of_manufacture = int(input(f'Year Of Manufacture (between {START_YEAR} and {END_YEAR}): '))      # integer [2016]
                         except ValueError:
-                            print('\033[0;31m\033[1mError: Please enter a valid integer.\033[0m')
+                            print('\033[0;31;1mError: Please enter a valid integer.\033[0m')
                             continue
 
                         if not (START_YEAR <= year_of_manufacture <= END_YEAR):
-                            print(f'\033[0;31m\033[1mError: Please enter a four digit year between {START_YEAR} and {END_YEAR}.\033[0m')
+                            print(f'\033[0;31;1mError: Please enter a four digit year between {START_YEAR} and {END_YEAR}.\033[0m')
                         else:
                             break
 
@@ -96,11 +96,11 @@ def CarSerStaff():
                         try:
                             seating_capacity = int(input(f'Seating Capacity (between {MIN_CAPACITY} and {MAX_CAPACITY} persons): '))   # integer [5 persons]
                         except ValueError:
-                            print('\033[0;31m\033[1mError: Please enter a valid integer.\033[0m')
+                            print('\033[0;31;1mError: Please enter a valid integer.\033[0m')
                             continue
 
                         if not (MIN_CAPACITY <= seating_capacity <= MAX_CAPACITY):
-                            print(f'\033[0;31m\033[1mError: Please enter a seating capacity between {MIN_CAPACITY} and {MAX_CAPACITY} persons.\033[0m')
+                            print(f'\033[0;31;1mError: Please enter a seating capacity between {MIN_CAPACITY} and {MAX_CAPACITY} persons.\033[0m')
                         else:
                             break
 
@@ -109,7 +109,7 @@ def CarSerStaff():
                     insurance_expiry_date = InputDate(f'Insurance Expiry Date (between {START_YEAR} and {END_YEAR} in DD-MM-YYYY): ')           # date [31-12-2024]
                     road_tax_expiry_date = InputDate(f'Road Tax Expiry Date (between {START_YEAR} and {END_YEAR} in DD-MM-YYYY): ')             # date [31-12-2024]
                     renting_rate = InputFloat('Renting Rate (per day): ')                                                                       # float [250.00]
-                    rental_availability = 'available'
+                    rental_availability = 'Available'                                                                                           # string [Available]
 
                     try:
                         with open(os.path.join(os.path.dirname(__file__), 'car_db.txt'), mode='a') as file: # 'a' - open a file for appending the text or creates a new file if the file does not exist
@@ -139,7 +139,7 @@ def CarSerStaff():
     [3] Expiry Date\n
     \033[1mRental\033[0m
     [4] Rate (per day)
-    [5] Availability \033[0;31m[In Development]\033[0m\n'''
+    [5] Availability\n'''
 
                     print(f'{pageCarUpdateOpt}')
 
@@ -153,7 +153,7 @@ def CarSerStaff():
                         option = input('>>> ').lower()
 
                         if option not in [CSS_UPDATE_INSNUM, CSS_UPDATE_INSDTE, CSS_UPDATE_TAXDTE, CSS_UPDATE_RTLDAY, CSS_UPDATE_RTLAVL, CSS_BACK]:
-                            print(f'\033[0;31m\033[1mError: Please enter {CSS_UPDATE_INSNUM}, {CSS_UPDATE_INSDTE}, {CSS_UPDATE_TAXDTE}, {CSS_UPDATE_RTLDAY}, {CSS_UPDATE_RTLAVL}, or {CSS_BACK} as an option.\033[0m')
+                            print(f'\033[0;31;1mError: Please enter {CSS_UPDATE_INSNUM}, {CSS_UPDATE_INSDTE}, {CSS_UPDATE_TAXDTE}, {CSS_UPDATE_RTLDAY}, {CSS_UPDATE_RTLAVL}, or {CSS_BACK} as an option.\033[0m')
                         else:
                             # Option 2.1 - Update Insurance Policy Number
                             if option == CSS_UPDATE_INSNUM:
@@ -249,7 +249,39 @@ def CarSerStaff():
 
                             # Option 2.5 - Update Rental Availability
                             if option == CSS_UPDATE_RTLAVL:
-                                print('5')
+                                os.system('cls')
+
+                                statusDict = {'1': 'Available', '2': 'Reserved', '3': 'Rented', '4': 'Under Service', '5': 'Disposed'}
+
+                                with open(os.path.join(os.path.dirname(__file__), 'car_db.txt'), mode='r') as file:
+                                    data = [line.split(';') for line in file.readlines()]
+
+                                for details in data:
+                                    if details[0] == export_registration_no:
+                                        pageOptRtlAvl = f'''\033[1mThe current Rental Availability for {export_registration_no} is {details[10]}\033[0m\n
+Select a new status:
+    [1] Available
+    [2] Reserved
+    [3] Rented
+    [4] Under Service
+    [5] Disposed\n'''
+
+                                        print(f'{pageOptRtlAvl}')
+
+                                        while True:
+                                            newRtlAvl = input('>>> ').lower()
+                                            if newRtlAvl in statusDict:
+                                                details[10] = statusDict[newRtlAvl]
+                                                break
+                                            else:
+                                                print('\033[0;31;1mError: Please enter 1, 2, 3, 4, or 5 as an option.\033[0m')
+
+                                with open(os.path.join(os.path.dirname(__file__), 'car_db.txt'), mode='w') as file:
+                                    file.writelines(';'.join(details) for details in data)
+                                    os.system('cls')
+
+                                return UpdateOptions(export_registration_no)
+
                             # Option 2.b - Go back
                             if option == CSS_BACK:
                                 os.system('cls')
@@ -279,7 +311,7 @@ def CarSerStaff():
                                     UpdateOptions(export_registration_no)
                                     break
                                 else:
-                                    print('\033[0;31m\033[1mError: No such car found.\033[0m')
+                                    print('\033[0;31;1mError: No such car found.\033[0m')
 
                     except FileNotFoundError:
                         print(f'{ERR_FILE_NOT_FOUND}')
@@ -309,7 +341,7 @@ def CarSerStaff():
                             while True:
                                 option = input('>>> ').lower()
                                 if option not in CSS_BACK:
-                                    print(f'\033[0;31m\033[1mError: Please enter {CSS_BACK} as an option.\033[0m')
+                                    print(f'\033[0;31;1mError: Please enter {CSS_BACK} as an option.\033[0m')
                                 elif option == CSS_BACK:
                                     os.system('cls')
                                     CarSerStaff()
